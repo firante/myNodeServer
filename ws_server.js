ac893b8831c517135161b17527fee1cbd9ef3f34
firante <misha.fedorovych@gmail.com>
2017-09-04 17:37:44 +0300

    create user

--- ws_server.js
+++ ws_server.js
@@ -3,6 +3,8 @@ const app = express();
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const router = express.Router();
+
+const bcrypt = require('bcryptjs');
 // --- creating users collection ---
 MongoClient.connect('mongodb://firante:rce15their@ds021691.mlab.com:21691/firantebase', (err, db) => {
   if(err) {
@@ -37,7 +39,6 @@ app.use(function (req, res, next) {
 });
 
 router.get('/', function(req, res) {
-  res.send('Hello Seattle\n');
 });
 
 router.route('/login')
@@ -47,5 +48,27 @@ router.route('/login')
     res.send('true');
   });
 
-app.use('/api', router);
+router.route('/register')
+  .post((req, res) => {
+    const SECRET_HASH = process.env.SECRET_HASH;
+    const { username, password, email } = req.body.body;
+    MongoClient.connect('mongodb://firante:rce15their@ds021691.mlab.com:21691/firantebase', (err, db) => {
+      if(err) {
+	console.log(err);
+      } else {
+	db.findOne({ "profile.email": email }, (err, user) => {
+	  if(err) res.status(500).send('Server Error! Registration failed!');
+	  if(user) {
+	    res.status(200).send({ message: 'Email is excist!' });
+	  } else {
+	    db.insert({ profile: { username, email, hash: bcrypt.hashSync(password, SECRET_HASH) } });
+	  }
+	});
+      }
+    });
+
+    res.send('true');
+  });
+
+app.use('/', router);
 app.listen(9999);
